a488f4f63bc6d3b53b781c6004d133bca7e63051
Thomas Simonson <thomas.simonson@gmail.com>
2020-04-24 00:59:28 +0200

    -typos in var names corrected (e.g obs->nobs) -treatment of missing values -setwd problem resolved -subsetting dataframe problems resolved (threashold, and NA before correlation) -problem w/read.csv was because I forgot to call the filenames function (resolved)

--- corr.R
+++ corr.R
@@ -1,20 +1,23 @@
 find.ID.aboveThr<-function(directory,threashold){
-        complete(directory)
-        nrComplete_aboveThr<-nrComplete_Stations[nrComplete_Stations>threashold]
-        ID.aboveThr<-nrComplete_aboveThr$obs
+        df.nrComplete<-complete(directory) #table of the stations and the corresponding number of complete observations.
+        aboveThr<-df.nrComplete$nobs>threashold #logical vector signalling the stations with a nr of complete obs above threashold
+        df.mostComplete<-df.nrComplete[aboveThr,] #above threashold subset
+        ID.aboveThr<-df.mostComplete$id #vector with the IDs of the selected stations
 }
 
+
 corr<-function(directory,threashold=0){
-        
+        fileNrs<-find.ID.aboveThr(directory,threashold)
+        fileNames<-filenames(fileNrs)
+        corrVector<-rep(NA,length(fileNames))        
         oldDir<-getwd() # The working directory must be the directory containing the data folder
         newDir<-paste(oldDir,"/",directory,sep="")
         setwd(newDir) # We set the working directory to the data folder itself
-        
-        fileNames<-find.ID.aboveThr(directory,threashold)
-        corrVector<-rep(NA,length(ID.aboveThr))
-        for(i in seq_along(ID.aboveThr)){
+        for(i in seq_along(fileNames)){
                 data<-read.csv(fileNames[i])
-                corrVector[i]<-cor(data$sulfate,data$nitrate)
+                comp<-complete.cases(data) #logical vector signalling the complete rows
+                compdf<-data[comp,] #complete rows subset
+                corrVector[i]<-cor(compdf$sulfate,compdf$nitrate)
         }
         setwd(oldDir)
         corrVector
